96c783318ca5dca92ca4ca92f5f77a5af185f40d
ranjan2503ravi <ranjan2503ravi@gmail.com>
2025-09-11 23:09:53 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import React, { useState } from "react";
+import React from 'react'
+import Movie from './Component/Movie';
 
-
-export default function PasswordGenerator() {
-const [password, setPassword] = useState("");
-
-
-const generatePassword = () => {
-const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*";
-let newPass = "";
-for (let i = 0; i < 10; i++) {
-const randomIndex = Math.floor(Math.random() * chars.length);
-newPass += chars[randomIndex];
+const App = () => {
+ 
+  return (
+    <div>
+<Movie/>
+    </div>
+  )
 }
-setPassword(newPass);
-};
 
-
-return (
-<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-<h1 className="text-xl font-bold mb-4">Simple Password Generator</h1>
-<input
-type="text"
-value={password}
-readOnly
-className="border px-3 py-2 rounded mb-3 w-64 text-center"
-placeholder="Click Generate"
-/>
-<button
-onClick={generatePassword}
-className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
->
-Generate Password
-</button>
-</div>
-);
-}
+export default App
